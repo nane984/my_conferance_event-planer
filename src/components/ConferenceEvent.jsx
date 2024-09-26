a02dcdc1a883c985ffd7f0fd202c14357dbeb906
nane984 <br.scekic@gmail.com>
2024-09-26 13:10:50 +0200

    add increment,  decrement and totalcost

--- src/components/ConferenceEvent.jsx
+++ src/components/ConferenceEvent.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { useState } from "react";
 import './ConferenceEvent.css'
 import { useSelector, useDispatch } from "react-redux";
+import { incrementQuantity, decrementQuantity } from "/src/venueSlice";
 
 function ConferenceEvent(){
   
+    const [showItems, setShowItems] = useState(false);
     const venueItems = useSelector((state) => state.venue);
+    const dispatch = useDispatch();
+
+    const handleToggleItems = () => {
+        console.log("handleToggleItems called");
+        setShowItems(!showItems);
+    };
+
+
+    const handleAddToCart = (index)=>{
+        dispatch(incrementQuantity(index));
+    }
+
+    const handleRemoveToCart = (index)=>{
+        dispatch(decrementQuantity(index));
+    }
+
+    const calculateTotalCost = () =>{
+        let totalCostLocal = 0;
+        venueItems.forEach(item => {
+            totalCostLocal += item.quantity * item.cost;
+        });
+        return totalCostLocal;
+    }
+
+    const totalCost = calculateTotalCost();
+
 
     return<>
         <navbar className="navbar_event_conference">
@@ -15,27 +44,45 @@ function ConferenceEvent(){
                     <a href="#addons">Add-ons</a>
                     <a href="#meals">Meals</a>
                 </div>
-                <button className="details_button">
+                <button className="details_button" onClick={handleToggleItems}>
                         Show Details
                 </button>
             </div>
         </navbar>
-        <div id="venue" className="venue_container container_main">
-            <div className="main_container">
-                <div className="text"> 
-                    <h1>Venue Room Selection</h1>
-                </div>
-                <div className="venue_selection">
-                {venueItems.map((item, index) => (
-            <div className="venue_main" key={index}>
-              <div className="img">
-                <img src={item.img} alt={item.name} />
-              </div>
-              </div>
-                ))}
-            </div>
+
+        <div className="main_container">
+            {!showItems ? 
+                (
+                    <div className="items-information">
+                        <div id="venue" className="venue_container">
+                            <div className="text">
+                                <h1>Venue Room Selection</h1>
+                            </div>
+                            <div className="venue_selection">
+                                {venueItems.map((item, index) => (
+                                    <div className="venue_main" key={index}>
+                                        <div>
+                                            <img className="img" src={item.img} alt={item.name} />
+                                        </div>
+                                        <div className="item_name">{item.name}</div>
+                                        <div className="item_price">${item.cost}</div>
+                                        <div className="button_container">
+                                            <button className={item.quantity === 0?("btn_disabled btn"):("btn_minus btn")}
+                                                onClick={() => handleRemoveToCart(index)}>&#8211;</button> 
+                                            <span>{item.quantity}</span>
+                                            <button className={item.quantity > item.max_quantity?("btn_disabled btn"):("btn_plus btn")}
+                                                onClick={() => handleAddToCart(index)}>&#43;</button>
+                                        </div>
+                                    </div>
+                                   ))} 
+                            </div>           
+                        </div>
+                    </div> ) : <p>Nije prikazano nista</p> }
+
+                    <div className="total_cost">Total Cost: $ {totalCost}</div>             
         </div>
-        </div>
+
+                
     </>
 };
 export default ConferenceEvent;
--- src/venueSlice.js
+++ src/venueSlice.js
@@ -9,34 +9,53 @@ export const venueSlice = createSlice({
       name: "Conference Room (Capacity:15)",
       cost: 3500,
       quantity: 0,
+      max_quantity: 7,
     },
     {
       img: "https://pixabay.com/images/download/event-venue-1597531_640.jpg",
       name: "Auditorium Hall (Capacity:200)",
       cost: 5500,
       quantity: 0,
+      max_quantity: 6,
     },
     {
       img: "https://pixabay.com/images/download/convention-center-3908238_640.jpg",
       name: "Presentation Room (Capacity:50)",
       cost: 700,
       quantity: 0,
+      max_quantity: 5,
     },
     {
       img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
       name: "Large Meeting Room (Capacity:10)",
       cost: 900,
       quantity: 0,
+      max_quantity: 4,
     },
     {
       img: "https://pixabay.com/images/download/laptops-593296_640.jpg",
       name: "Small Meeting Room (Capacity:5)",
       cost: 1100,
       quantity: 0,
+      max_quantity: 3,
     },
   
-  ],reducers: {},
+  ],reducers: {
+      incrementQuantity(state, action){
+        const {payload: index} = action
+        if(state[index] && (state[index].quantity <= state[index].max_quantity)){
+          state[index].quantity++;
+        }
+      },
+
+      decrementQuantity(state, action){
+        const {payload: index} = action
+        if(state[index] && (state[index].quantity > 0)){
+          state[index].quantity--;
+        }
+      }
+  },
 });
 
-export const { } = venueSlice.actions;
+export const {incrementQuantity, decrementQuantity} = venueSlice.actions;
 export default venueSlice.reducer;
